a1fcef37beb8e48fc65a350744eb81e8ce0763b3
stevenmlee91 <stevenmlee91@gmail.com>
2026-04-17 19:35:53 -0700

    Initial IOS project set up

---
+++ ProjectPR/ProjectPR/ProjectPRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPRApp.swift
+//  ProjectPR
+//
+//  Created by Steven Lee on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
